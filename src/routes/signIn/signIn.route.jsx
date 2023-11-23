bcfc89d692c452bf10ad7941c9fcab8003fd6670
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-11-24 00:14:50 +0530

    sign in with google provider

--- src/routes/signIn/signIn.route.jsx
+++ src/routes/signIn/signIn.route.jsx
@@ -1,7 +1,72 @@
+import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../../context/user.Context";
+import "./signIn.style.scss";
+import trendSpotterLogo from "/trendSpotterLogo.svg";
+import { FaGoogle } from "react-icons/fa";
+import { useEffect } from "react";
 const SignIn = () => {
+  // import database from context
+  const { user, googleHandler } = useUserContext();
+
+  // navigate to home
+  let naviagte = useNavigate();
+  const logoutToHome = () => {
+    naviagte("/");
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    googleHandler();
+  };
+
+  useEffect(() => {
+    if (user) {
+      logoutToHome();
+    }
+  }, [user]);
+
   return (
-    <div>
-      <h1>SignIn</h1>
+    <div className="signIn-container">
+      {/* image section */}
+      <div className="image-container"></div>
+      {/* form section */}
+      <div className="form-container">
+        <span className="logo-container">
+          <img src={trendSpotterLogo} alt="logo" className="logo" />{" "}
+          <h1>Welcome</h1>
+        </span>
+        <p className="message">
+          Do not have an account? <span className="unique">Create today!</span>
+        </p>
+
+        <form className="form" onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Enter your email address"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your valid password"
+            />
+          </div>
+          <div className="form-submit">
+            <button type="submit">Sign In</button>
+            <button type="submit">
+              <FaGoogle />
+              Sign In with Google
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
