9743d483ff388bfe313397acf5c7fe4b7cadff25
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-11-24 00:27:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/main.scss";
+// wrapp the app with provider
 import { UserContextProvider } from "./context/user.Context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
